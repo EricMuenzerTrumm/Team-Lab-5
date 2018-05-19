5e70676de32d3e9c6184f358b3cf1d9bacc565d5
MachineManiaIII <4879415+MachineManiaIII@users.noreply.github.com>
2018-05-18 22:21:55 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,63 @@
+/***********************************************************************************
+Header:
+************************************************************************************
+Author: Morgan Anderson, Eric Muenzer-Trumm
+Organization: Rogue Community College
+File Name: Main.cpp
+Date Created: May 18, 2018
+Modifications: 05-22-18 12:00
+***********************************************************************************
+
+***********************************************************************************
+Documentation:
+***********************************************************************************
+Lab Assignment: Lab 5 - Structured Files (Group)
+
+Abstract: This program reads and writes structures to a file.
+
+Input: Cstrings, Integers
+
+Output: Write to File
+***********************************************************************************/
+
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
-#include <iomanip>
-#include <cstring>
-#include <string>
 #include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+
 using std::cout;
 using std::cin;
 using std::endl;
 using std::fstream;
 using std::ios;
 using std::fixed;
-using std::setprecision;
 using std::setw;
-using std::left;
+using std::setprecision;
+
+// Prototypes
+void Menu();
+void DisplayFile();
+void RetrieveData();
+void AddRecord();
+void ChangeRecord();
+void DeleteRecord();
+void DisplayRecord();
+
+// Main function
+int main()
+{
+
+	system("pause");
+	return 0;
+}
+
+/**********************************************************************************
+Output:
+***********************************************************************************
+
+
+
+***********************************************************************************/
